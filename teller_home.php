6d5a1004f4a059de2acfc3abdaaa1cdb7168b886
Mehrab <mk.zizou@gmail.com>
2018-05-23 18:17:57 +0800

    Add scanner to bank teller

--- teller_home.php
+++ teller_home.php
@@ -23,10 +23,42 @@ session_start();
 
   <!-- Custom CSS -->
   <link href="css/login.css" rel="stylesheet">
+
+  <!-- Custom Scanner CSS -->
+  <link rel="stylesheet" href="css/scannerStyle.css">
+
 </head>
 
 <?php include "header.php"; ?>
 <body>
+  <div id="qr-app">
+    <div class="sidebar">
+      <section class="cameras">
+        <h2>Cameras</h2>
+        <ul>
+          <li v-if="cameras.length === 0" class="empty">No cameras found</li>
+          <li v-for="camera in cameras">
+            <span v-if="camera.id == activeCameraId" :title="formatName(camera.name)" class="active">{{ formatName(camera.name) }}</span>
+            <span v-if="camera.id != activeCameraId" :title="formatName(camera.name)">
+              <a @click.stop="selectCamera(camera)">{{ formatName(camera.name) }}</a>
+            </span>
+          </li>
+        </ul>
+      </section>
+      <section class="scans">
+        <h2>Scans</h2>
+        <ul v-if="scans.length === 0">
+          <li class="empty">No scans yet</li>
+        </ul>
+        <transition-group name="scans" tag="ul">
+          <li v-for="scan in scans" :key="scan.date" :title="scan.content">{{ scan.content }}</li>
+        </transition-group>
+      </section>
+    </div>
+    <div class="preview-container">
+      <video id="scanner"></video>
+    </div>
+  </div>
 
 
 <!-- jQuery -->
@@ -38,6 +70,12 @@ session_start();
 <!--Validator -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/1000hz-bootstrap-validator/0.11.9/validator.min.js"></script>
 
+<!--webcam scanner -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/webrtc-adapter/3.3.3/adapter.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.1.10/vue.min.js"></script>
+<script type="text/javascript" src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js"></script>
+
+<script type="text/javascript" src="js/scannerApp.js"></script>
 
 </body>
 
