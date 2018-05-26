39fb885918768b0b88a1cd7c9bd276d18841664d
Umar Adkhamov <35759152+UmarAdkham@users.noreply.github.com>
2018-05-26 10:46:00 +0800

    Huh? should be some spaces changed. Forced me to push

--- createService.php
+++ createService.php
@@ -15,30 +15,30 @@ $fieldType = $_POST['field_type'];
 $sql_bank = "SELECT bank.bankID from bank, branch WHERE bank.bankID = branch.bankID and branchID='$branchID'";
   $result_bank = $conn->query($sql_bank);
 
-
+  
   while ($rs_bank = $result_bank->fetch_array()) {
      $_SESSION['bankID'] = $rs_bank['bankID'];
  }
  $bankID = $_SESSION['bankID'];
-
+ 
 //Variables for data_type table
 $sql = "INSERT INTO service (serviceName, bankID, description) VALUES ('$name', '$bankID', '$desc')";
 
 //Getting auto inserted ID of the service
-if ($conn->query($sql) === TRUE)
+if ($conn->query($sql) === TRUE) 
 {
 	$last_service_id = $conn->insert_id;
 
 //Inserting values into data_type table from checkboxes
 	$numOfCheckBoxes = count($common_fields);
-	for ($i=0; $i < $numOfCheckBoxes; $i++) {
+	for ($i=0; $i < $numOfCheckBoxes; $i++) { 
 		${"sqlBridge$i"} = "INSERT INTO service_data_type VALUES ('$last_service_id', '$common_fields[$i]')";
 		$conn -> query(${"sqlBridge$i"});
 	}
 
 //Inserting values into data_type table from POSTed array
 	$numOfFields = count($fieldName);
-	for ($i=0; $i < $numOfFields; $i++) {
+	for ($i=0; $i < $numOfFields; $i++) { 
 		if ($fieldName[$i] != NULL) {
 			${"sql$i"} = "INSERT INTO data_type (fieldName, fieldType) VALUES ('$fieldName[$i]', '$fieldType[$i]')";
 			if ($conn->query(${"sql$i"}) === TRUE)
@@ -56,8 +56,7 @@ if ($conn->query($sql) === TRUE)
 	echo "<script type='text/javascript'>alert('$message');
 	window.location.href = 'superAdmin.php';
 	</script>";
-
-//	window.location.href = 'superAdmin.php';
+	
 
 } else {
 	echo "Error: " . $sql . "<br>" . $conn->error;
